3e50bfebdca1cb139f26f75a62fe92423bff52c8
kjxbyz <kjxbyz888@outlook.com>
2024-05-28 21:09:53 +0800

    initial commit

---
+++ Casks/fafarunner.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "fafarunner" do
+  version "1.0.0+286"
+  # https://docs.brew.sh/Cask-Cookbook#stanza-sha256
+  sha256 :no_check
+
+  # https://docs.brew.sh/Cask-Cookbook#stanza-url
+  # https://docs.brew.sh/Cask-Cookbook#when-url-and-homepage-domains-differ-add-verified
+  url "https://github.com/fafarunner/fafarunner/releases/download/v#{version}/FaFa.Runner.dmg",
+      verified: "github.com/fafarunner/fafarunner/"
+  name "FaFa Runner"
+  desc "A game developed using flame & bonfire."
+  homepage "https://chenyifaer.com/fafarunner"
+
+  # Documentation: https://docs.brew.sh/Brew-Livecheck
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:\.\d+)+)(\+\d+)?/i)
+    strategy :github_latest
+  end
+
+  # https://docs.brew.sh/Cask-Cookbook#stanza-depends_on
+  depends_on macos: ">= :catalina"
+  depends_on arch: [:x86_64]
+
+  # https://docs.brew.sh/Cask-Cookbook#stanza-app
+  app "FaFa Runner.app"
+
+  # https://docs.brew.sh/Cask-Cookbook#stanza-uninstall
+  # uninstall quit: "com.kjxbyz.watermarkassistant.mac"
+
+  # Documentation: https://docs.brew.sh/Cask-Cookbook#stanza-zap
+  zap trash: []
+end
---
+++ README.md
@@ -0,0 +1,20 @@
+# homebrew-fafarunner
+
+[![brew test-bot](https://github.com/fafarunner/homebrew-fafarunner/actions/workflows/tests.yml/badge.svg?branch=main)](https://github.com/fafarunner/homebrew-fafarunner/actions/workflows/tests.yml)
+
+## Install
+
+`brew install fafarunner/fafarunner/fafarunner`
+
+Or `brew tap fafarunner/fafarunner` and then `brew install fafarunner`.
+
+Or, in a [`brew bundle`](https://github.com/Homebrew/homebrew-bundle) `Brewfile`:
+
+```ruby
+tap "fafarunner/fafarunner"
+brew "fafarunner"
+```
+
+## Documentation
+
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
